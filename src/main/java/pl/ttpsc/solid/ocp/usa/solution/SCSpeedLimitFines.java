d3058025eb93a99d1b5987a4377301c8edd804c7
Mateusz Wiśniewski <siwypl@go2.pl>
2018-03-29 21:02:41 +0200

    Ex. 3 open-closed responsibility principle solution done

---
+++ src/main/java/pl/ttpsc/solid/ocp/usa/solution/SCSpeedLimitFines.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.ttpsc.solid.ocp.usa.solution;
+
+/**
+ *
+ * @author Siwy
+ */
+public class SCSpeedLimitFines implements USASpeedLimitFines{
+    
+    private static final int MAX_SPEED = 0;
+    
+    @Override
+    public double calcualateSpeedLimitFine(int speed) {
+        double fine = 0;
+        if (speed > MAX_SPEED) {
+                // calculate...
+        }
+        return fine;
+    }
+}
